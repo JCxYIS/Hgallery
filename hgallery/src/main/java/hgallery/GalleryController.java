884821348c70bf77b0b94f07ba95d170a647bfaa
JCxYIS (DIYPC-2020) <jsn811@gmail.com>
2020-06-06 20:19:56 +0800

    can print album

--- hgallery/src/main/java/hgallery/GalleryController.java
+++ hgallery/src/main/java/hgallery/GalleryController.java
@@ -1,16 +1,64 @@
 package hgallery;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import hgallery.Debug.ConsoleColor;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.RowConstraints;
 
-public class GalleryController implements Initializable
+public class GalleryController 
 {
-    @Override
-    public void initialize(URL location, ResourceBundle resources)
+    @FXML private GridPane grid;
+
+
+    @FXML
+    private void initialize()
     {
-        MainController.instance.SetTitle("相簿");
+        Resize();
+    }
+
+    /**
+     * 重新製作大小
+     */
+    @FXML
+    private void Resize()
+    {
+        Debug.Log("RESIZING GALLERY...");
+        int albumCount = 41;
+        
+        for(int i = 0; i < albumCount; i++)
+        {
+            int col = i%3;
+            int row = i/3;            
+            
+            // new row
+            if(col == 0 && row != 0)
+            {
+                grid.setPrefHeight(grid.getPrefHeight() + 300);
+                grid.getRowConstraints().add(new RowConstraints(300)); // column 2 is 200 wide
+                //Debug.Log(grid.getPrefHeight());
+            }
+
+            try
+            {
+                Node newItem = FXMLLoader.load(getClass().getResource("_GalleryFraction.fxml"));
+                grid.add(newItem, col, row);
+            }
+            catch (IOException e)
+            {
+                Debug.Log(e, ConsoleColor.RED);
+            }
+        }
     }
 }
