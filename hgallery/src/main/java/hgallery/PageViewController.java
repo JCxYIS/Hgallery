38e01455b45bccd8ef66ec1686a208bb08047b6a
JCxYIS <jsn811@gmail.com>
2020-06-09 17:19:31 +0800

    可以縮放/滾動頁面

--- hgallery/src/main/java/hgallery/PageViewController.java
+++ hgallery/src/main/java/hgallery/PageViewController.java
@@ -8,32 +8,42 @@ import hgallery.Debug.ConsoleColor;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.AnchorPane;
 
 public class PageViewController {
-    @FXML
-    private AnchorPane ap;
-    @FXML
-    private Button butt_r, butt_l;
-    @FXML
-    private ImageView img;
-    @FXML
-    private ImageView icon_l, icon_r;
+    @FXML private AnchorPane ap;
+    @FXML private Button butt_r, butt_l;
+    @FXML private ImageView img;
+    @FXML private ImageView icon_l, icon_r;
+    @FXML private Label lab_page, lab_name, lab_status;
 
     /**
      * 圖片檔案s
      */
     private File[] files;
     /**
-     * 這可能不是實際頁數，是檔案的次序。
+     * 這可能不是實際頁數，是圖片檔案的次序。
      */
-    private int page;
+    private int page = 0;
+    /**
+     * 
+     */
+    private String albumName = "";
+    /**
+     * 圖片縮放係數
+     */
+    private double scaleMultipler = 1;
+    /**
+     * 圖片 Y 偏移
+     */
+    private double deltaY = 1;
 
 
 
-    
     /**
      * 設定這個view的參數s
      */
@@ -47,7 +57,7 @@ public class PageViewController {
                 return AlbumFileReader.IsValidPicType(pathname);
             }
         });
-        page = 0;
+        albumName = galleryDir.getName();
     }
 
     @FXML
@@ -77,24 +87,32 @@ public class PageViewController {
         icon_l.setLayoutY(h/2 - 30);
         icon_r.setLayoutY(h/2 - 30);
 
-        img.setFitWidth (w - 20);
-        img.setFitHeight(h - 20);
+        img.setFitWidth (w - 60);
+        img.setFitHeight(h - 60);
+
+        img.setScaleX(scaleMultipler);
+        img.setScaleY(scaleMultipler);
+        img.setTranslateY(deltaY);
     }
 
     @FXML
     private void NextPage() 
     {
         if(page < files.length-1)
+        {
             page++;
-        SwitchPage();
+            SwitchPage();
+        }
     }
 
     @FXML
     private void PrevPage() 
     {
         if(page > 0)
+        {
             page--;
-        SwitchPage();
+            SwitchPage();
+        }
     }
 
     private void SwitchPage() 
@@ -112,7 +130,30 @@ public class PageViewController {
         catch (Exception e)
         {
             Debug.Log("無法顯示圖片："+e, ConsoleColor.RED);
+            lab_status.setText("無法顯示");
         }
+
+        lab_status.setText("");
+        lab_page.setText( (page+1)+" / "+(files.length) );
+        lab_name.setText( albumName );
+    }
+
+    @FXML 
+    private void onScroll(ScrollEvent event)
+    {
+        double scrollAmount = event.getDeltaY();
+
+        if(event.isControlDown())
+        {
+            scaleMultipler += 0.006 * scrollAmount;
+            Debug.Log("CTRL + onScroll | ScaleMultipler="+scaleMultipler, ConsoleColor.BLUE);
+        }
+        else
+        {
+            deltaY += 1 * scrollAmount;
+            Debug.Log("onScroll | deltaY="+deltaY, ConsoleColor.BLUE);
+        }
+        Resize();
     }
 
 }
