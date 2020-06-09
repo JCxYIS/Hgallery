c4e271fd62469b568bbbab7cc29a83bc27190b0e
JCxYIS <jsn811@gmail.com>
2020-06-10 01:16:36 +0800

    修正小bug

--- hgallery/src/main/java/hgallery/App.java
+++ hgallery/src/main/java/hgallery/App.java
@@ -19,7 +19,7 @@ import hgallery.Settings.Settings;
  */
 public class App extends Application 
 {
-    public static final String version = "v.1.45";
+    public static final String version = "v.1.46";
 
 
     private static Scene scene;
